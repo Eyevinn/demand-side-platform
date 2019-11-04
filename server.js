6d5aa59bb4a746ab3d2bc3c70d58f5fc9ee3ff7f
birme <jonas.birme@eyevinn.se>
2019-11-04 11:17:14 +0100

    Ability to provide ad server URL with env variable

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const DSPEngine = require('./index.js');
 const MockAgent = require('./agents/mock.js');
 
 const engine = new DSPEngine();
-let url = "http://" + (process.env.MYIP || 'localhost') + ":" + (process.env.PORT || 8081);
+let url = process.env.ADSERVER_URL || "http://" + (process.env.MYIP || 'localhost') + ":" + (process.env.PORT || 8081);
 engine.addAgent(new MockAgent({ adServerUrl: url }));
 engine.listen(process.env.PORT || 8081, process.env.MYIP || 'localhost');
