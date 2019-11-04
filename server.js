2e1ebccf33c381fa2215526ec0ab0a1a8c260a76
birme <jonas.birme@eyevinn.se>
2019-11-04 11:19:15 +0100

    Providing bind IP messes with heroku

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const MockAgent = require('./agents/mock.js');
 const engine = new DSPEngine();
 let url = process.env.ADSERVER_URL || "http://" + (process.env.MYIP || 'localhost') + ":" + (process.env.PORT || 8081);
 engine.addAgent(new MockAgent({ adServerUrl: url }));
-engine.listen(process.env.PORT || 8081, process.env.MYIP || 'localhost');
+engine.listen(process.env.PORT || 8081);
